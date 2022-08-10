4a2031d634dc0b33882bc455241f059bc5a7bf0e
Artem <artem.v.artemenko@gmail.com>
2022-08-10 22:10:11 +0300

    created vue router

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Index
+        <router-link :to="{name: 'fruit.index'}">List</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
